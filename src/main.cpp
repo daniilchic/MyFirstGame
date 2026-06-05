a01ca7bf57b295d3c0d80410d5da7d088512a062
n1lo <daniarumiantsev@ya.ru>
2026-06-05 19:29:01 +0300

    add glad lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(){
@@ -16,7 +18,17 @@ int main(){
 
     glfwMakeContextCurrent(window);
 
+    if(!gladLoadGL()){
+        std::cout << "Can't load Glad" << std::endl;
+	return -1;
+    }
+
+    std::cout << "OpenGL " << GLVersion.major << "," << GLVersion.minor << std::endl;
+
+    glClearColor(0, 1, 0, 1);
+
     while(!glfwWindowShouldClose(window)){
+	glClear(GL_COLOR_BUFFER_BIT);
 
 	glfwSwapBuffers(window);
 
