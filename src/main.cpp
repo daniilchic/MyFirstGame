7283e65f8d45a2f6ddeccbe08d46a7eb221824a0
n1lo <daniarumiantsev@ya.ru>
2026-06-06 14:01:03 +0300

    add esc and now possible to set window size

--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,65 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height);
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode);
+
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
 int main(){
 
     if(!glfwInit()){
+	std::cout << "glfwInir failed" << std::endl;
         return -1;
     }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Game", NULL, NULL);
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Game", nullptr, nullptr);
     
-    if(!window){
+    if(!pWindow){
+	std::cout << "glfwCreateWindow failed" << std::endl;
         glfwTerminate();
 	return -1;
     }
 
-    glfwMakeContextCurrent(window);
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
+    glfwSetKeyCallback(pWindow, glfwKeyCallback);
+    glfwMakeContextCurrent(pWindow);
 
     if(!gladLoadGL()){
         std::cout << "Can't load Glad" << std::endl;
 	return -1;
     }
 
-    std::cout << "OpenGL " << GLVersion.major << "," << GLVersion.minor << std::endl;
-
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
     glClearColor(0, 1, 0, 1);
 
-    while(!glfwWindowShouldClose(window)){
+    while(!glfwWindowShouldClose(pWindow)){
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glfwSwapBuffers(window);
+	glfwSwapBuffers(pWindow);
 
 	glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
+    glfwDestroyWindow(pWindow);
     glfwTerminate();
 
     return 0;
+}
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height){
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+}
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode){
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+    }
 }
