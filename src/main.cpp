df9d582e24925f7dcb23355ae5bcb190891c4e12
n1lo <daniarumiantsev@ya.ru>
2026-06-11 02:39:21 +0300

    add resource manager

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <GLFW/glfw3.h>
 
 #include "Renderer/ShaderProgram.h"
+#include "Resources/ResourceManager.h"
 
 GLfloat point[] = {
      0.0f,  0.5f, 0.0f,
@@ -17,23 +18,6 @@ GLfloat colors[] = {
     0.0f, 0.0f, 1.0f
 };
 
-const char* vertex_shader =
-"#version 460\n"
-"layout(location = 0) in vec3 vertex_position;"
-"layout(location = 1) in vec3 vertex_color;"
-"out vec3 color;"
-"void main(){"
-"    color = vertex_color;"
-"    gl_Position = vec4(vertex_position, 1.0);"
-"}";
-
-const char* fragment_shader =
-"#version 460\n"
-"in vec3 color;"
-"out vec4 frag_color;"
-"void main(){"
-"    frag_color = vec4(color, 1.0);"
-"}";
 
 void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height);
 void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode);
@@ -41,7 +25,8 @@ void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
 
-int main(){
+int main(int argc, char** argv){
+
 
     if(!glfwInit()){
 	std::cout << "glfwInir failed" << std::endl;
@@ -72,53 +57,54 @@ int main(){
     std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
     glClearColor(0, 0, 0, 1);
+    {
+        ResourceManager resourceManager(argv[0]);
+        auto pDefaultShaderProgram = resourceManager.loadShaders("DefaultShader", "res/shaders/vertex_shader.txt", "res/shaders/fragment_shader.txt"); 
+        if(!pDefaultShaderProgram){
+            std::cerr << "Can't create shader program: " << "DefaultShader" << std::endl;
+            return -1;
+        }
 
-    std::string vertexShader(vertex_shader);
-    std::string fragmentShader(fragment_shader);
-    Renderer::ShaderProgram shaderProgram(vertexShader, fragmentShader);
-    if(!shaderProgram.isCompiled()){
-        std::cerr << "Can't create shader program!" << std::endl;
-        return -1;
-    }
+        GLuint points_vbo = 0;
+        glGenBuffers(1, &points_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
 
-    GLuint points_vbo = 0;
-    glGenBuffers(1, &points_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+        GLuint colors_vbo = 0;
+        glGenBuffers(1, &colors_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 
-    GLuint colors_vbo = 0;
-    glGenBuffers(1, &colors_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-
-    GLuint vao = 0;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+        GLuint vao = 0;
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
     
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
     
-    while(!glfwWindowShouldClose(pWindow)){
-		glClear(GL_COLOR_BUFFER_BIT);
+        while(!glfwWindowShouldClose(pWindow)){
+		    glClear(GL_COLOR_BUFFER_BIT);
 	
-        shaderProgram.use();
-		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+            pDefaultShaderProgram->use();
+	    	glBindVertexArray(vao);
+	    	glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		glfwSwapBuffers(pWindow);
+		    glfwSwapBuffers(pWindow);
 
-		glfwPollEvents();
+		    glfwPollEvents();
+        }
     }
 
    	glfwDestroyWindow(pWindow);
-   	glfwTerminate();
+    glfwTerminate();
 
-   	return 0;
+  	return 0;
+    
 }
 
 void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height){
