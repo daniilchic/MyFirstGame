07dd6af572dbf206a99050531a650c03259ac656
n1lo <daniarumiantsev@ya.ru>
2026-06-26 13:22:28 +0300

    add matrix

--- src/Renderer/ShaderProgram.h
+++ src/Renderer/ShaderProgram.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <glad/glad.h>
 #include <string>
+#include <glm/mat4x4.hpp>
 namespace Renderer{
     	class ShaderProgram{
 		public:
@@ -9,7 +10,7 @@ namespace Renderer{
 	   		bool isCompiled() const { return m_isCompiled; }
 	 		void use() const;
             void setInt(const std::string& name, const GLint value);
-
+            void setMatrix4(const std::string& name, glm::mat4& matrix);
             ShaderProgram() = delete;
             ShaderProgram(ShaderProgram&) = delete;
             ShaderProgram& operator=(const ShaderProgram&) = delete;
--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,17 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/vec2.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "Renderer/ShaderProgram.h"
 #include "Resources/ResourceManager.h"
 #include "Renderer/Texture2D.h"
 
 GLfloat point[] = {
-     0.0f,  0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f
+     0.0f,  50.0f, 0.0f,
+     50.0f, -50.0f, 0.0f,
+     -50.0f, -50.0f, 0.0f
 }; 
 
 GLfloat colors[] = {
@@ -105,13 +108,28 @@ int main(int argc, char** argv){
         
         pDefaultShaderProgram->use();
         pDefaultShaderProgram->setInt("tex", 0);
-            
+          
+        glm::mat4 modelMatrix_1 = glm::mat4(1.f);
+        modelMatrix_1 = glm::translate(modelMatrix_1, glm::vec3(100.f, 200.f, 0.f));
+
+        glm::mat4 modelMatrix_2 = glm::mat4(1.f);
+        modelMatrix_2 = glm::translate(modelMatrix_2, glm::vec3(590.f, 200.f, 0.f));
+
+        glm::mat4 projectionMatrix = glm::ortho(0.f, static_cast<float>(g_windowSize.x), 0.f, static_cast<float>(g_windowSize.y), -100.f, 100.f);
+
+        pDefaultShaderProgram -> setMatrix4("projectionMat", projectionMatrix);
+        
         while(!glfwWindowShouldClose(pWindow)){
 		    glClear(GL_COLOR_BUFFER_BIT);
 	
             pDefaultShaderProgram->use();
 	    	glBindVertexArray(vao);
             tex->bind();
+
+            pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_1);
+	    	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_2);
 	    	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		    glfwSwapBuffers(pWindow);
